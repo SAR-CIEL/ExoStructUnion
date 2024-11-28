bd34fb31e4243edc7a21f721bd125c13bce057f9
Mairbek Saralapov <saralapovmairbek21@gmail.com>
2024-11-28 09:30:16 +0100

    Ajoutez des fichiers projet.

---
+++ GestionEmployes/gestionEmp.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "ICI LA GESTION EMPLOYES";
+	return 0;
+}
---
+++ RGB/rgb.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "ICI LA RGB";
+	return 0;
+}
